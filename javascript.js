3f32d3196221c2c9b793a6400b9900582cd74b4e
AlexanderAhlfors <alexanderahlfors@axxell.fi>
2021-11-04 12:50:34 +0200

    fixed added buttons

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 /*function changePage(){
     .innerHTML z-index ="99999"
 } */
+var x = document.getElementById("hiddenP").value = "0";
+
 
 $("#page1").hover(function(){
     $("#page1").style.zIndex = "9999";
     $("#page2").style.zIndex = "1";
     $("#page3").style.zIndex = "1";
     $("#page4").style.zIndex = "1";
+    x.value = "1";
+    
 });
 
 $("#page2").hover(function(){
@@ -14,6 +18,7 @@ $("#page2").hover(function(){
     $("#page2").style.zIndex = "99999";
     $("#page3").style.zIndex = "1";
     $("#page4").style.zIndex = "1";
+    x.value = "2";
 });
 
 $("#page3").hover(function(){
@@ -21,6 +26,7 @@ $("#page3").hover(function(){
     $("#page2").style.zIndex = "1";
     $("#page3").style.zIndex = "999999";
     $("#page4").style.zIndex = "1";
+    x.value = "3";
 });
 
 $("#page4").hover(function(){
@@ -28,4 +34,5 @@ $("#page4").hover(function(){
     $("#page2").style.zIndex = "1";
     $("#page3").style.zIndex = "1";
     $("#page4").style.zIndex = "999999";
+    x.value = "4";
 });
